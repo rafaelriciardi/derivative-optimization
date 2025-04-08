034bdfb8ef994c82c9d59950cc3526119119af67
Rafael Riciardi <rafael.r.barreira@hotmail.com>
2025-04-08 03:21:10 +0000

    Mantido no seletor somente as opcoes pedidas

--- streamlit_app.py
+++ streamlit_app.py
@@ -62,7 +62,7 @@ with col2:
 
 option = st.selectbox(
     "Qual código você deseja executar?",
-    ("Otimização Personalizada", "Otimização Gaussiana", "Otimização Linear", "Intervalo Positivo", "Otimização Retorno Positivo"),
+    ("Otimização Personalizada", "Otimização Retorno Positivo"),
 )
 
 #####################################################################
